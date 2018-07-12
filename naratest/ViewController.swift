70ae6c754d07d06b089a5d9914b3136f1a20a06a
Narawith <wingdi@gmail.com>
2018-07-12 14:05:15 +0700

    Initial Commit

---
+++ naratest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  naratest
+//
+//  Created by NaRaWiTh on 12/7/2561 BE.
+//  Copyright © 2561 wingdiapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
